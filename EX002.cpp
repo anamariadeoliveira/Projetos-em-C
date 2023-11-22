311de13917d8ce25b46c9502d739e4a8fb8b274a
Ana Maria de Oliveira <117085967+anamariadeoliveira@users.noreply.github.com>
2023-11-22 16:08:50 -0300

    Add files via upload

---
+++ EX002.cpp
@@ -0,0 +1,16 @@
+// Exercicio do curso de Linguagem em C pela Udemy.
+//
+
+#include <iostream>
+
+int main()
+{
+    
+
+    printf("MEU \n\n");
+    printf("\tSEGUNDO \n\n");
+    printf("\t\tPROGRAMA\n\n");
+    printf("\t\t\tEM LINGUAGEM C \n\n");
+    return 0;
+}
+
