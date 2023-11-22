25600dc4bb606e43e7b2e2f0e1a2196646140ce6
Ana Maria de Oliveira <117085967+anamariadeoliveira@users.noreply.github.com>
2023-11-22 16:08:12 -0300

    Add files via upload

---
+++ Exercicio01.cpp
@@ -0,0 +1,16 @@
+// Exercicio01.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <stdio.h>
+
+int main()
+{
+
+    printf("Meu nome e \"Ana\"\n");
+    printf("O nome da minha mae e Edlea\n");
+    printf("O nome da minha vo e Jacinta\n");
+
+   
+    
+
+}
